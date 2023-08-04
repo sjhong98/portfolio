4f7542d860e4d4a0d8fe90ac48f3e9458d52505b
sjhong98 <sjhong98@icloud.com>
2023-08-04 12:02:14 +0900

    feat: Redux

--- src/index.js
+++ src/index.js
@@ -5,38 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
-
-export const increase = () =>{
-    return {
-        type: 'INCREASE'
-    };
-};
-
-export const decrease = () =>{
-    return {
-        type: 'DECREASE'
-    };
-};
-
-
-
-const count = 1;
-
-const reducer = (state = count, action) => {
-    switch(action.type) {
-        case "INCREASE" :
-            return state + 1;
-        
-        case "DECREASE" :
-            return state - 1;
-        
-        case "SET" :
-            return action.payload;
-        
-        default:
-            return state;
-    }
-}
+import reducer from './reducers.js';
 
 const store = createStore(reducer);
 
